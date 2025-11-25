e1da591f609ae24a30c94b203563e4a93e3aca9a
mico_L <blurredmicol@gmail.com>
2025-11-25 20:02:47 +0100

    changes to fix build issues

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/carlo-ferretti-portfolio/', // <-- add this line
   build: {
     outDir: 'docs'
   }
